b31dba3b78d8b7e849a15f25ad1baecb62cdb779
bharath <bharath.narva09@gmail.com>
2021-11-30 23:40:24 +0000

    added new files to canteen_reservation_system

---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+    <!-- Footer Section Starts -->
+    <div class="footer">
+        <div class="wrapper">
+           <p class="text-center">2021 All rights reserved, School Canteen Reservation System. Developed by <a href="#"> Students</a></p>
+        </div>
+    </div>
+    <!-- Footer Section Ends -->
+</body>
+</html>
